6b04ecd7cea21affde01997e90c8f3e07659c356
mafalda-mgv <mafalda.mgv@gmail.com>
2021-08-09 22:04:08 +0200

    Initial commit

--- R/notas-prueba.R
+++ R/notas-prueba.R
@@ -10,11 +10,15 @@ usethis::edit_git_config() # usa este para windows no es lo que quiero
 # esto abre un archivo, le pones tu nombre y tu mail
 # hay que poner en NAME-- el nombre de ususario que tengo en github
 
-usethis::gitcreds::gitcreds_set() #da errorr no funciona
+gitcreds::gitcreds_set() #da errorr no funciona
 
-usethis::create_github_token() #  esto me abre el link
-usethis::use_git() # esto me funciona
+usethis::create_github_token() #  esto me abre el link, para crear un tocken
+#SOLO APARECE UNA VEZ
+
+usethis::use_git() # esto me funciona, me dice que si quiero comit un archivo
+
 usethis::use_github()
-usethis::edit_r_environ()
+
+usethis::edit_r_environ() #esto no o hafo yo
 
 
