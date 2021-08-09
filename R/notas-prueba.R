01f8617442c16caac7d54d5ead7ea3c3c557e420
Melisa Gomez Velazquez <mafalda.mgv@gmail.com>
2021-08-09 20:44:05 +0200

    Initial commit

---
+++ R/notas-prueba.R
@@ -0,0 +1,17 @@
+install.packages("gitcreds", "gert", "gh")
+
+library("gitcreds")
+library("gert")
+library("gh")
+
+gitcreds::gitcreds_set() #da errorr no funciona
+usethis::gitcredits # esto no me funciona
+usethis::edit_git_config() # usa este para windows no es lo que quiero
+
+usethis::gitcreds::gitcreds_set()
+usethis::use_git()
+usethis::use_github()
+
+usethis::create_github_token() # empezar a las 8:35 # esto me abre el link
+
+
