fff41f1df41dc7c951c0cfdb2c78e165bba63025
mafalda-mgv <mafalda.mgv@gmail.com>
2021-08-09 20:54:32 +0200

    Initial commit

--- R/notas-prueba.R
+++ R/notas-prueba.R
@@ -7,11 +7,13 @@ library("gh")
 gitcreds::gitcreds_set() #da errorr no funciona
 usethis::gitcredits # esto no me funciona
 usethis::edit_git_config() # usa este para windows no es lo que quiero
+# esto abre un archivo, le pones tu nombre y tu mail
+# hay que poner en NAME-- el nombre de ususario que tengo en github
 
-usethis::gitcreds::gitcreds_set()
-usethis::use_git()
-usethis::use_github()
+usethis::gitcreds::gitcreds_set #da errorr no funciona
 
 usethis::create_github_token() # empezar a las 8:35 # esto me abre el link
+usethis::use_git() # esto me funciona
+usethis::use_github()
 
 
